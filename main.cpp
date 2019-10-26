5bd4d63d1576e973e41c1a71ebac402b6ddf446d
BHN16 <dario.toribio@utec.edu.pe>
2019-10-26 14:36:08 -0500

    A

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 #include "Graph.h"
 #include <vector>
 int main(){
-	Graph<double,false> g("pp.vtk");
+	Graph<double,false> g("100points.vtk");
 	g.print_adjacency_list();
-	g.delete_node(10,127);
-	g.delete_edge(10,10,127,10);
-	g.print_adjacency_list();
+	g.delete_node(531,616);
+	g.delete_node(451,656);
+	g.delete_node(767,613);
+	//g.delete_edge(10,10,127,10);
+	/*g.print_adjacency_list();
 	std::cout << g.densidad() << std::endl;
 	std::cout << g.connected() << std::endl;	
-	//g.bipartited();
 	std::cout << g.BFS(10,10) << std::endl;
+	g.add_node(5000,5000);
+	g.add_edge()*/
+	//g.add_node(500,500);
+	std::cout << g.grade_of_node(638,206) << std::endl;
+	std::cout << g.connected() << std::endl;
+	//g.print_adjacency_list();
 	return 0;
 }
